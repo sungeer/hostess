249641f5c36d459a6a662edf7e368037ea5ad870
wangxun <1013296326@qq.com>
2026-08-14 15:54:59 +0800

    fix

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,10 @@
-import sys
+from pathlib import Path
 
 from loguru import logger
+
+base_dir = Path(__file__).resolve().parent.parent
+
+log_path = base_dir / 'logs/app_{time:YYYY-MM-DD}.log'
 
 
 def setup_logger():
@@ -9,11 +13,14 @@ def setup_logger():
     fmt = '{time:YYYY-MM-DD HH:mm:ss} - {level} - {name}:{function}:{line} - {message}'
 
     logger.add(
-        sink=sys.stdout,
+        log_path,
+        rotation='16:00',
+        retention='7 days',
         format=fmt,
+        encoding='utf-8',
         diagnose=False,
         backtrace=False,
         colorize=False,
-        enqueue=False,
+        enqueue=False,  # 关闭异步记录
         level='INFO',
     )
